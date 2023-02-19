1ab82a6f6b059da3c0d2a93c4beab6daa8885325
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-02-18 21:01:12 -0300

    Salvando usuários em memória (Headers), JSON

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 // const http = require('http');
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+  const { method, url } =  req;
+  
+  if(method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    });
+
+    return res.end('User successfully created!');
+  }
+
   res.end('Server on fire!');
 });
 
