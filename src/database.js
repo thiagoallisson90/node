6cf9008294f0be54a5db968d070d82074044c952
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-02-22 16:41:28 -0300

    Middlewares

---
+++ src/database.js
@@ -0,0 +1,16 @@
+export class Database {
+  database =  {};
+
+  insert(table, data) {
+    if(Array.isArray(this.database[table])) {
+      this.database[table].push(data);
+    } else {
+      this.database[table] = data;
+    }
+    return data;
+  }
+
+  select(table) {
+    return this.database[table] ?? [];
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 // const http = require('http');
 import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { Database } from './database.js';
 
-const users = [];
+const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } =  req;
   
+  await json(req, res);
+
   if(method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-type', 'application/json')
@@ -13,10 +17,11 @@ const server = http.createServer((req, res) => {
   }
 
   if(method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name,
+      email
     });
 
     return res.writeHead(201).end();
