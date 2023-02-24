456b0d643ed618e8792059a9ac81442980fdd8c2
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-02-24 15:15:35 -0300

    Data update

--- src/database.js
+++ src/database.js
@@ -33,6 +33,14 @@ export class Database {
     return this.#database[table] ?? [];
   }
 
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    if(rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1, data);
+      this.#persist();
+    }
+  }
+
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id);
     if(rowIndex > -1) {
